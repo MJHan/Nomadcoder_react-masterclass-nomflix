4e0cf4538950814796295f5427a957c7288eb71b
MJHan <hamatoto@gmail.com>
2023-08-11 17:06:47 +0900

    Nomflix Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Home from "./Routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,6 @@ body {
   background-color:${(props) => props.theme.bgColor};
   color:${(props) => props.theme.textColor};
   line-height: 1.2;
-  background: linear-gradient(135deg, #e09, #d0e);
 }
 a {
   text-decoration:none;
